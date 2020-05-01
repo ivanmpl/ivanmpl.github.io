32c75a2d0ad33112113a1df370b887d204e8d9a3
Ivan <ivanmp@live.com>
2020-04-30 22:32:14 -0600

    add guess number post and update date format

--- scripts/main.js
+++ scripts/main.js
@@ -10,9 +10,10 @@ $(document).ready(function () {
 
 });
 
+// TODO: Use moment.js for date formatting
 function addPost(post) {
     $("<article/>", {
         "class": "blog-post",
-        html: ("<h2>" + post.title + "</h2>" + "<h5>" + new Date(post.date) + "</h5>" + "<p>" + post.content + "</p>" + "<p>" + "<a href=" + post.link + ">Implementation on Github </a>" + "</p>")
+        html: ("<h2>" + post.title + "</h2>" + "<h5>" + (new Date(post.date).getMonth() + 1) + "/" + new Date(post.date).getDate() + "/" + new Date(post.date).getFullYear() + "</h5>" + "<p>" + post.content + "</p>" + "<p>" + "<a href=" + post.link + ">Implementation on Github </a>" + "</p>")
     }).appendTo(".blog-post-section");
 }
